0ca8903ea91cb7a38ce2b2ee15339bee298a2d5e
kongbuhaja <pcar530@gmail.com>
2022-12-16 17:33:23 +0900

    ver2

--- line_detector/main.cpp
+++ line_detector/main.cpp
@@ -39,6 +39,7 @@ void video(std::string filepath) {
 	while (1) {
 		if (!cap.isOpened()) {
 			std::cerr << "can't open" << std::endl;
+			return;
 		}
 		cap >> frame;
 		if (frame.empty()) {
@@ -53,8 +54,9 @@ void video(std::string filepath) {
 		cv::rectangle(frame, cv::Point(ex_lines[0][1], 380+ex_lines[0][2]), cv::Point(ex_lines[0][1] + ex_lines[0][3], 380+ex_lines[0][2] + ex_lines[0][4]), cv::Scalar(255, 0, 255), 2);
 		cv::rectangle(frame, cv::Point(ex_lines[1][1], 380+ex_lines[1][2]), cv::Point(ex_lines[1][1] + ex_lines[1][3], 380+ex_lines[1][2] + ex_lines[1][4]), cv::Scalar(255, 255, 0), 2);
 		
+		cv::circle(frame, cv::Point(pos.first, 400), 1, cv::Scalar(255, 255, 255), 2);
+		cv::circle(frame, cv::Point(pos.second, 400), 1, cv::Scalar(255, 255, 255), 2);
 		cv::imshow("video", frame);
-		cv::imshow("roi_th_gray", roi_th_gray);
 	
 		if (cv::waitKey(25) == ' ') {
 			break;
@@ -85,7 +87,7 @@ void preprocessing(const cv::Mat& frame, cv::Mat &dst) {
 	cv::Scalar mean = cv::mean(frame);
 	double m = (mean[0] + mean[1] + mean[2]) / 3;
 	dst = dst + (dst - m) * 1.0;
-	cv::Mat th, YCrCb, Y, Cr, Cb, gray, Yth, dst_gray;
+	cv::Mat dst_gray;
 	std::vector<cv::Mat> sp;
 	cv::cvtColor(dst, dst_gray, cv::COLOR_BGR2GRAY);
 	cv::threshold(dst_gray, dst, 110, 255, cv::THRESH_BINARY_INV);
@@ -93,15 +95,6 @@ void preprocessing(const cv::Mat& frame, cv::Mat &dst) {
 	cv::GaussianBlur(dst, dst, cv::Size(3, 3), 0);
 	cv::GaussianBlur(dst, dst, cv::Size(3, 3), 0);
 	cv::threshold(dst, dst, 120, 255, cv::THRESH_BINARY);
-	cv::GaussianBlur(dst, dst, cv::Size(3, 3), 0);
-	cv::GaussianBlur(dst, dst, cv::Size(3, 3), 0);
-	cv::threshold(dst, dst, 120, 255, cv::THRESH_BINARY);
-	cv::GaussianBlur(dst, dst, cv::Size(3, 3), 0);
-	cv::GaussianBlur(dst, dst, cv::Size(3, 3), 0);
-	cv::threshold(dst, dst, 120, 255, cv::THRESH_BINARY);
-
-	cv::imshow("roi", frame);
-	cv::imshow("gray_th", dst);
 }
 
 std::pair<int, int> connect_component(const cv::Mat& frame, std::vector<std::vector<int>>& ex_lines) {
@@ -173,14 +166,6 @@ std::pair<int, int> connect_component(const cv::Mat& frame, std::vector<std::vec
 	}
 	ex_lines[0] = left_line;
 	ex_lines[1] = right_line;
-
-	cv::cvtColor(dst, dst, cv::COLOR_GRAY2BGR);
-	cv::rectangle(dst, cv::Point(left_line[1], left_line[2]), cv::Point(left_line[1] + left_line[3], left_line[2] + left_line[4]), cv::Scalar(255, 255, 0), 1);
-	cv::rectangle(dst, cv::Point(right_line[1], right_line[2]), cv::Point(right_line[1] + right_line[3], right_line[2] + right_line[4]), cv::Scalar(255, 255, 0), 1);
-	cv::circle(dst, cv::Point(left_line[1] + left_line[3] / 4, 20), 1, cv::Scalar(0, 0, 0),2);
-	cv::circle(dst, cv::Point(right_line[1] + right_line[3] / 4*3, 20), 1, cv::Scalar(0, 0, 0),2);
-
-	cv::imshow("component", dst);
 
 	return { left_line[1] + left_line[3] / 4, right_line[1] + right_line[3] / 4 * 3 };
 }
